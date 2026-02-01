632450ed48f31d686bcc280196d45f8e16ddb2e6
Vimlesh Mourya <vimlesh@gmail.com>
2026-02-01 18:49:06 +0530

    Add second Azure resource group for AKS in main.tf

--- Infra/module/rg/main.tf
+++ Infra/module/rg/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-aks-01"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks-02"
+  location = "West Europe"
+}

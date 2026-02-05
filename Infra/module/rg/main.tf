f7621a59392d9dc875aca40a3edbd149ef41ce68
vimleshmourya <kumar.vimlesh93@gmail.com>
2026-02-05 13:32:34 +0530

    Add second Azure resource group 'rg2'

--- Infra/module/rg/main.tf
+++ Infra/module/rg/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-aks-01"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg-aks-02"
+  location = "West Europe"
+}

e6779146d16adcfc26fab183c5a704813a74bc7b
vimleshmourya <kumar.vimlesh93@gmail.com>
2026-02-05 11:57:02 +0530

    Update main.tf

--- Infra/module/rg/main.tf
+++ Infra/module/rg/main.tf
@@ -2,7 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-aks-01"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-aks-02"
-  location = "West Europe"
-}

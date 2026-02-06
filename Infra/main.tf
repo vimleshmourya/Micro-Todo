dee50973bc658349a60e7516aefa56e425def736
vimleshmourya <kumar.vimlesh93@gmail.com>
2026-02-06 13:54:39 +0530

    Comment out infrastructure module definitions
    Comment out multiple module definitions in main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,30 +2,30 @@
   #source = "./module/rg"
 #}
 
- module "vnet" {
-   depends_on = [module.rg]
-   source     = "./module/vnet"
- }
+ #module "vnet" {
+   #depends_on = [module.rg]
+   #source     = "./module/vnet"
+ #}
 
- module "subnet" {
-   depends_on = [module.vnet]
-   source     = "./module/subnet"
- }
+ #module "subnet" {
+   #depends_on = [module.vnet]
+   #source     = "./module/subnet"
+ #}
 
- module "piblicIP" {
-   depends_on = [module.rg]
-   source     = "./module/publicIP"
- }
+ #module "piblicIP" {
+   #depends_on = [module.rg]
+   #source     = "./module/publicIP"
+ #}
 
- module "appgw" {
-   depends_on = [module.subnet]
-   source     = "./module/appgw"
- }
+ #module "appgw" {
+   #depends_on = [module.subnet]
+   #source     = "./module/appgw"
+ #}
 
- module "aks" {
-   depends_on = [module.rg, module.appgw]
-   source     = "./module/aks"
- }
+ #module "aks" {
+   #depends_on = [module.rg, module.appgw]
+   #source     = "./module/aks"
+ #}
 
  module "acr" {
    depends_on = [module.rg]

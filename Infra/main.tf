8fc2956dd5dbc7e9d63890a729a0ff8209bf0a6e
vimleshmourya <kumar.vimlesh93@gmail.com>
2026-02-12 22:06:16 +0530

    Comment out all module configurations
    Comment out all module definitions in main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,38 +1,38 @@
-module "rg" {
-  source = "./module/rg"
-}
+#module "rg" {
+ # source = "./module/rg"
+#}
 
- module "vnet" {
-   depends_on = [module.rg]
-   source     = "./module/vnet"
- }
+ #module "vnet" {
+  # depends_on = [module.rg]
+   #source     = "./module/vnet"
+ #}
 
- module "subnet" {
-   depends_on = [module.vnet]
-   source     = "./module/subnet"
- }
+ #module "subnet" {
+  # depends_on = [module.vnet]
+   #source     = "./module/subnet"
+ #}
 
- module "piblicIP" {
-   depends_on = [module.rg]
-   source     = "./module/publicIP"
- }
+ #module "piblicIP" {
+  # depends_on = [module.rg]
+   #source     = "./module/publicIP"
+ #}
 
- module "appgw" {
-   depends_on = [module.subnet]
-   source     = "./module/appgw"
- }
+ #module "appgw" {
+  # depends_on = [module.subnet]
+   #source     = "./module/appgw"
+ #}
 
- module "aks" {
-   depends_on = [module.rg, module.appgw]
-   source     = "./module/aks"
- }
+ #module "aks" {
+  # depends_on = [module.rg, module.appgw]
+   #source     = "./module/aks"
+ #}
 
- module "acr" {
-   depends_on = [module.rg]
-   source     = "./module/acr"
- }
+ #module "acr" {
+  # depends_on = [module.rg]
+   #source     = "./module/acr"
+ #}
 
- module "sqldb" {
-   depends_on = [module.rg]
-   source     = "./module/sqldb"
- }
+ #module "sqldb" {
+  # depends_on = [module.rg]
+   #source     = "./module/sqldb"
+ #}
